eadfc6775742cb75b33a97967c564158469b3f08
Matheus Medeiros <matheusmedeiroswf@gmail.com>
2024-06-08 14:45:08 -0300

    Implementação do serviço de envio de E-mail.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quartz;
+using System.Threading.Tasks;
+using InvestmentPortfolioManagement.Jobs;
+using InvestmentPortfolioManagement.Tests;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+
+        // Chama o teste SMTP
+        //SmtpTest.RunTest();
+
+        var host = CreateHostBuilder(args).Build();
+
+        var scheduler = await host.Services.GetRequiredService<ISchedulerFactory>().GetScheduler();
+
+        var job = JobBuilder.Create<EmailNotificationJob>()
+            .WithIdentity("emailNotificationJob", "group1")
+            .Build();
+
+        var trigger = TriggerBuilder.Create()
+            .WithIdentity("emailNotificationTrigger", "group1")
+            .WithCronSchedule("0 */5 * * * ?") // Executa a cada 5 minutos
+            .Build();
+
+        await scheduler.ScheduleJob(job, trigger);
+
+        await host.RunAsync();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
